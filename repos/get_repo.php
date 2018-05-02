7255734e5dbc0e0e7bc2a5aa87f99ee27bbe44fc
Tim Williams <tmw@autotrain.org>
2018-05-02 13:58:22 +0100

    Remove youtube and mdlinternal repo types, these were never  in active use and won't be supported now. More code checker fixes.

--- repos/get_repo.php
+++ repos/get_repo.php
@@ -14,29 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+defined('MOODLE_INTERNAL') || die();
+
 function get_repomanager_instance($repoi, $course) {
     switch ($repoi->get_meta()->type) {
         case "coursefilearea":
-            require_once ("coursefilearea.php");
+            require_once("coursefilearea.php");
             return new block_repofile_coursefilearea($course);
         case "local":
         case "recent":
         case "user":
-            require_once ("generic.php");
+            require_once("generic.php");
             return new block_repofile_generic($repoi);
         case "filesystem":
-            require_once ("filesystem.php");
+            require_once("filesystem.php");
             return new block_repofile_filesystem($repoi);
-            /*
-            case "user":
-            require_once("mdlinternal.php");
-            return new block_repofile_mdlinternal($repoi);
-            
-            case "youtube":
-            require_once("youtube.php");
-            return new block_repofile_youtube($repoi);
-            */
     }
-    require_once ('block_repofile_type.php');
+    require_once('block_repofile_type.php');
     return new block_repofile_type();
 }
